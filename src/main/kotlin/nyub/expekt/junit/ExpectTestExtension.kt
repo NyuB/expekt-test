8215a85b46908a95778f0f88d18d600df806bd3c
Brice Decaestecker <brice.decaestecker@gmx.fr>
2025-02-20 06:38:36 +0100

    Improve JUnit extension documentation

--- src/main/kotlin/nyub/expekt/junit/ExpectTestExtension.kt
+++ src/main/kotlin/nyub/expekt/junit/ExpectTestExtension.kt
@@ -10,11 +10,70 @@ import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.api.extension.ParameterContext
 import org.junit.jupiter.api.extension.ParameterResolver
 
+/**
+ * JUnit5 [extension](https://junit.org/junit5/docs/current/user-guide/#extensions) automating expect tests setup.
+ * Provides [ExpectTests.ExpectTest] parameters, and asserts that the provided [ExpectTests.ExpectTest]'s printed
+ * content is consumed during the test (see @[ExpectUnhandledOutput] to change this behaviour).
+ *
+ * Example:
+ * ```java
+ *  @ExtendWith(ExpectTestExtension.class)
+ *  @ExpectTestExtension.RootPath("src/test/kotlin")
+ *  class Test {
+ *   @Test
+ *   void test(ExpectTests.ExpectTest t) {
+ *     t.print("Ok");
+ *     t.expect("""
+ *     Ok
+ *     """);
+ *   }
+ *  }
+ * ```
+ *
+ * ### Configuration
+ *
+ * Each parameter is configurable by a system property or by annotation. Annotations take precedence over system
+ * properties.
+ *
+ * #### Promotion (in-place update) of expected string blocks
+ * - default: `false`
+ * - system property: [PROMOTE_PROPERTY_KEY]
+ * - annotation: [Promote]
+ *
+ * #### Classes root path
+ * - default: `"src/test/kotlin"`
+ * - system property: [CLASSES_ROOT_PROPERTY_KEY]
+ * - annotation: [RootPath]
+ */
 class ExpectTestExtension : ParameterResolver, BeforeEachCallback, AfterEachCallback, BeforeAllCallback {
-    private var promote: Boolean = System.getProperty("nyub.expekt.promote", "false") == "true"
-    private var root: Path = System.getProperty("nyub.expekt.root", "src/test/kotlin").let(::Path)
+    /**
+     * Do not fail if the [ExpectTests.ExpectTest] output buffer is not empty at the end of the test
+     *
+     * @throws AssertionError if the [ExpectTests.ExpectTest] output buffer is empty at the end of the test
+     */
+    @Target(AnnotationTarget.FUNCTION) annotation class ExpectUnhandledOutput
+
+    /** Override system property for promotion behavior */
+    @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION) annotation class Promote(val value: Boolean)
+
+    /** Override system property for sources root path */
+    @Target(AnnotationTarget.CLASS) annotation class RootPath(val value: String)
+
+    private var promote: Boolean = System.getProperty(PROMOTE_PROPERTY_KEY, "false") == "true"
+    private var root: Path = System.getProperty(CLASSES_ROOT_PROPERTY_KEY, "src/test/kotlin").let(::Path)
     private lateinit var expectTests: ExpectTests
     private lateinit var expectTest: ExpectTests.ExpectTest
+
+    companion object {
+        /** Set to `"true"` to trigger promotion */
+        const val PROMOTE_PROPERTY_KEY = "nyub.expekt.promote"
+
+        /**
+         * Path of the test source code from test run folder, should lead to the last folder before package folders,
+         * e.g. `"src/test/java"` for the usual maven setup.
+         */
+        const val CLASSES_ROOT_PROPERTY_KEY = "nyub.expekt.root"
+    }
 
     override fun beforeAll(ctx: ExtensionContext) {
         ctx.testClass.ifPresent {
@@ -59,17 +118,4 @@ class ExpectTestExtension : ParameterResolver, BeforeEachCallback, AfterEachCall
         }
         expectTest.end()
     }
-
-    /**
-     * Do not fail if the [ExpectTests.ExpectTest] output buffer is not empty at the end of the test
-     *
-     * @throws AssertionError if the [ExpectTests.ExpectTest] output buffer is empty at the end of the test
-     */
-    @Target(AnnotationTarget.FUNCTION) annotation class ExpectUnhandledOutput
-
-    /** Override system property for promotion behavior */
-    @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION) annotation class Promote(val value: Boolean)
-
-    /** Override system property for sources root path */
-    @Target(AnnotationTarget.CLASS) annotation class RootPath(val value: String)
 }
