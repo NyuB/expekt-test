76547b2ce1231f3d05c80c81223fd1c474868cf4
Brice Decaestecker <brice.decaestecker@gmx.fr>
2025-03-06 06:14:13 +0100

    Add shell example

---
+++ src/test/kotlin/nyub/expekt/demos/ShellTest.kt
@@ -0,0 +1,60 @@
+package nyub.expekt.demos
+
+import nyub.expekt.ExpectTests
+import org.junit.jupiter.api.Test
+
+class ShellTest {
+
+    @Test
+    fun `shell demo`() =
+        ExpectTests(promote = true).expectTest {
+            val shell = Shell(::camelCase, ::print)
+            shell("AlreadyCamelCase")
+            shell("snake_case")
+            shell("pascal-case")
+            shell("just capitalize this")
+            expect(
+                """
+        $ AlreadyCamelCase
+        AlreadyCamelCase
+        $ snake_case
+        SnakeCase
+        $ pascal-case
+        PascalCase
+        $ just capitalize this
+        Just Capitalize This
+        """
+                    .trimIndent()
+            )
+        }
+
+    class Shell(private val handler: (String) -> String, private val printer: (String) -> Unit) {
+        operator fun invoke(input: String) {
+            printer("$ $input\n")
+            val output = handler(input)
+            printer("$output\n")
+        }
+    }
+
+    private fun camelCase(s: String): String {
+        val res = StringBuilder()
+        var bump = true
+        s.forEach {
+            when {
+                it.isWhitespace() -> {
+                    bump = true
+                    res.append(it)
+                }
+                it == '-' || it == '_' -> {
+                    bump = true
+                }
+                it.isLetter() && bump -> {
+                    res.append(it.uppercase())
+                    bump = false
+                }
+                it.isLetter() -> res.append(it)
+            }
+        }
+        return res.toString()
+    }
+}
